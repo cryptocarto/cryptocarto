2af2c44d742a1f470f0a01723941c8fa31b7607d
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2019-10-29 17:44:22 +0100

    Files for first smart contract deployment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cryptocarto
 CryptoCarto main repository
+
+
+# Deploy smart contract
+
+- insert valid private key in truffle-config.json
+- run `truffle deploy --reset --network baobab`
+
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,27 @@
+const CryptoCarto = artifacts.require('./CryptoCarto.sol')
+const fs = require('fs')
+
+module.exports = function (deployer) {
+  deployer.deploy(CryptoCarto)
+    .then(() => {
+    // Record recently deployed contract address to 'deployedAddress' file.
+    if (CryptoCarto._json) {
+      // Save abi file to deployedABI.
+      fs.writeFile(
+        'deployedABI',
+        JSON.stringify(CryptoCarto._json.abi, 2),
+        (err) => {
+          if (err) throw err
+          console.log(`The abi of ${CryptoCarto._json.contractName} is recorded on deployedABI file`)
+        })
+    }
+
+    fs.writeFile(
+      'deployedAddress',
+      CryptoCarto.address,
+      (err) => {
+        if (err) throw err
+        console.log(`The deployed contract address * ${CryptoCarto.address} * is recorded on deployedAddress file`)
+    })
+  })
+}
