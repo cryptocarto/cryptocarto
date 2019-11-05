3bf8ac6e463ed4217cc8d653f6d929940219e0bc
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2019-11-05 21:50:36 +0100

    Running blockchain calls in parallel

--- index.js
+++ index.js
@@ -12,40 +12,50 @@ app.use('/leaflet', express.static(__dirname + '/leaflet')); // redirect leaflet
 app.use('/js', express.static(__dirname + '/js')); // redirect js
 app.use('/css', express.static(__dirname + '/css')); // redirect css
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use(require('body-parser').urlencoded({ extended: true }));
 
+//Setting up Klaytn data
+const Caver = require('caver-js')
+const caver = new Caver('https://api.cypress.klaytn.net:8651/')
+
+// Deployed smart contract address
+const smartContractAddress = process.env.SMART_CONTRACT_ADDRESS
+
+// Get the ABI
+const fs = require('fs')
+const deployedAbi = fs.readFileSync('deployedABI', 'utf8')
+
+// Setting Cryptocarto account for pulls
+// const account = caver.klay.accounts.wallet.add(process.env.KLAYTN_WALLET_KEY_FOR_CALLS)
+
+// Setting contract
+const CryptoCartoContract = deployedAbi
+  && smartContractAddress
+  && new caver.klay.Contract(JSON.parse(deployedAbi), smartContractAddress)
 
 //Define routes
 app.get('/',
 async function(req, res, next) {
   try {
-    const Caver = require('caver-js')
-    const caver = new Caver('https://api.cypress.klaytn.net:8651/')
-
-    // Deployed smart contract address
-    const smartContractAddress = process.env.SMART_CONTRACT_ADDRESS
-
-    // Get the ABI
-    const fs = require('fs')
-    const deployedAbi = fs.readFileSync('deployedABI', 'utf8')
-    
-    // Setting Cryptocarto account for pulls
-    const account = caver.klay.accounts.wallet.add(process.env.KLAYTN_WALLET_KEY_FOR_CALLS)
-    
-    // Setting contract
-    const CryptoCartoContract = deployedAbi
-      && smartContractAddress
-      && new caver.klay.Contract(JSON.parse(deployedAbi), smartContractAddress)
-    
     // Getting pin data
     CryptoCartoContract.methods.getAllPinTokenIds().call().then(async function(tokenIds){
-        var allTokensData = new Array;
-        for(tokenId of tokenIds) {
-            tokenDataReturned = await CryptoCartoContract.methods.getPinToken(tokenId).call()
-            allTokensData[tokenDataReturned[0]] = tokenDataReturned;
-        }
 
-        // Render view
-        res.render('index', { allTokensData: allTokensData, tokenIds: tokenIds });
+        // Getting token data in parallel
+        const tokenDataPromises = tokenIds.map(async function(tokenId){
+          return CryptoCartoContract.methods.getPinToken(tokenId).call()
+        });
+
+        // Waiting for execution and retrieving
+         await Promise.all(tokenDataPromises).then(function(result){
+          var allTokensData = new Array;
+          result.forEach(tokenData => {
+            allTokensData[tokenData[0]] = tokenData;
+          })
+
+          // Render view
+           res.render('index', { allTokensData: allTokensData, tokenIds: tokenIds });
+         })
+
     })
   } catch (error) { next(error) }
 });
