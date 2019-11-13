e8cf7fe1a3d86fa5f0f1f82e3766fa38c3f24ac6
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2019-11-13 15:35:08 +0100

    Functionality to import an existing PK

--- index.js
+++ index.js
@@ -151,6 +151,7 @@ async function(req, res, next) {
   } catch (error) { next(error) }
 });
 
+// Create a new transaction
 app.post('/new-transaction',
 async function(req, res, next) {
   try {
@@ -200,6 +201,30 @@ async function(req, res, next) {
 
   } catch (error) { next(error) }
 });
+
+// Import an existing private key
+app.post('/import-pk',
+async function(req, res, next) {
+  try {
+    const privatekey    = req.body.newprivatekey;
+
+    // Retrieve account for this private key
+    retrievedAccount = await caver.klay.accounts.privateKeyToAccount(privatekey);
+
+    // Set session variables
+    req.session.address = retrievedAccount.address;
+    req.session.privatekey = retrievedAccount.privateKey;
+
+    req.session.generalMessage = 'Account with address ' + retrievedAccount.address.substring(0,10) + '... was succesfully retrieved.';
+    res.redirect('/');
+  } catch (error) { next(error) }
+});
+
+//Error handler
+app.use(function (err, req, res, next) {
+  req.session.generalMessage = "Error: " + err.message;
+  res.redirect('/');
+})
 
 app.listen(port, function(){
 	console.log("CryptoCarto now UP on port "+port);
