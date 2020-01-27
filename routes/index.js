3d2302d8cce0185226431882a8ee9d4db78946d2
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2020-01-27 18:37:44 +0100

    Refactored view-pin route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var router = express.Router();
 
 // Route listing
+router.get('/view-pin/:pinid', require('./view-pin'));
 router.post('/transfer-pin', require('./transfer-pin'));
 router.get('/save-user-info', require('./save-user-info'));
 router.get('/token-metadata/:tokenid', require('./token-metadata'));
