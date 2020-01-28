c2ee0bdb48589be94a04134570af814439272412
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2020-01-28 12:49:09 +0100

    Refactored updatePinTokensDB() function

--- index.js
+++ index.js
@@ -94,52 +94,8 @@ setInterval(function() {
   updatePinTokensDB();
 }, 30000); // 30sec
 
-// Function to update PinToken DB from blockchain
-updatePinTokensDB = async function () {
-  await CryptoCartoContract.methods.getAllPinTokenIds().call().then(async function(tokenIds){
-
-    // Exclude known Pins from lookup (pins already in DB)
-    tokenIdsToLookup = tokenIds;
-    currentPinIds = await PinToken.distinct("tokenId");
-    tokenIdsToLookup = tokenIds.filter(x => !currentPinIds.includes(x));
-    
-    // If new tokens, save them in DB
-    if (tokenIdsToLookup.length > 0) {
-
-      // Getting token data in parallel
-      const tokenDataPromises = tokenIdsToLookup.map(async function(tokenId){
-        return CryptoCartoContract.methods.getPinToken(tokenId).call()
-      });
-
-      // Waiting for blockchain return, then saves to DB
-      await Promise.all(tokenDataPromises).then(async function(result) {
-
-        // Saves new tokens to DB
-        const tokenSavePromises = Object.keys(result).map(async function (objectKey) {
-          var newPinToken = new PinToken({
-            tokenId : result[objectKey][0],
-            owner: result[objectKey][1],
-            latitude : result[objectKey][2],
-            longitude : result[objectKey][3],
-            message : result[objectKey][4],
-            timestamp : result[objectKey][5]
-          });
-        
-          // Saves if not existing
-          if (!await PinToken.countDocuments({ tokenId: newPinToken.tokenId })) {
-            await newPinToken.save();
-            console.log("PinToken #" + newPinToken.tokenId + " saved to DB.")
-          };
-          
-        })
-        // Wait for saves
-        await Promise.all(tokenSavePromises)
-      })
-    }
-  });
-}
-
 // Triggers an update at app launch
+updatePinTokensDB = require('./utils/update-pin-tokens-db')
 updatePinTokensDB();
 
 //Define routes
