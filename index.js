4dd3da491a24ea54f76a4d06e388518125b15115
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2020-01-28 15:22:13 +0100

    Refactored general message middleware

--- index.js
+++ index.js
@@ -35,36 +35,8 @@ if (process.env.ENVIRONMENT != 'dev') {
   })
 }
 
-// General message middleware
-var messageMiddleware = function (req, res, next) {
-  if (typeof req.session.generalMessage != 'undefined') {
-    res.locals.generalMessage = req.session.generalMessage;
-    req.session.generalMessage = "";
-  }
-  
-  if (typeof req.session.address != 'undefined') {
-    res.locals.address = req.session.address;
-  }
-  
-  if (typeof req.session.privatekey != 'undefined') {
-    res.locals.privatekey = req.session.privatekey;
-  }
-  
-  if (typeof req.session.currentlat != 'undefined') {
-    res.locals.currentlat = req.session.currentlat;
-  }
-  
-  if (typeof req.session.currentlng != 'undefined') {
-    res.locals.currentlng = req.session.currentlng;
-  }
-
-  if (typeof req.session.openPinId != 'undefined') {
-    res.locals.openPinId = req.session.openPinId;
-    req.session.openPinId = "";
-  }
-  next()
-}
-app.use(messageMiddleware)
+// Add general message middleware
+app.use(require('./utils/message-middleware'))
 
 //Get a configured caver instance
 const caver = require('./utils/caver')
