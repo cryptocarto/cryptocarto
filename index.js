04c0bfaa7ae6bbde95b03da3ecf93d9de494c39a
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2020-01-28 12:40:50 +0100

    Refactored import-pk route

--- index.js
+++ index.js
@@ -336,24 +336,6 @@ async function(req, res, next) {
   } catch (error) { next(error) }
 });
 
-// Import an existing private key
-app.post('/import-pk',
-async function(req, res, next) {
-  try {
-    const privatekey    = req.body.newprivatekey;
-
-    // Retrieve account for this private key
-    retrievedAccount = await caver.klay.accounts.privateKeyToAccount(privatekey);
-
-    // Set session variables
-    req.session.address = retrievedAccount.address;
-    req.session.privatekey = retrievedAccount.privateKey;
-
-    req.session.generalMessage = 'Account with address ' + retrievedAccount.address.substring(0,10) + '... was succesfully retrieved.';
-    res.redirect('/');
-  } catch (error) { next(error) }
-});
-
 // View a specific pin
 app.get('/view-pin/:pinid',
 async function(req, res, next) {
