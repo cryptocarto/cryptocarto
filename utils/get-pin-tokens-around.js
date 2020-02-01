adc5f64d4107fe393c1553501971b228e8989344
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2020-02-01 14:07:56 +0100

    Sort token list by distance to center

--- utils/get-pin-tokens-around.js
+++ utils/get-pin-tokens-around.js
@@ -12,7 +12,20 @@ module.exports = async function (latitude, longitude, userAddress) {
       longitude: { $lt: longitude*10000+1000, $gt: longitude*10000-1000}
     };
 
-    var tokensDataFromDB = await PinToken.find(params).sort({timestamp:-1});
+    // Get tokens sorted by distance from center
+    var tokensDataFromDB = await PinToken.find(params);
+    tokensDataFromDB.sort(function(doc1, doc2) { 
+      doc1DistanceToCenter = Math.sqrt(
+        Math.pow(Math.abs(Math.abs(doc1.latitude) - Math.abs(latitude*10000)), 2) + 
+        Math.pow(Math.abs(Math.abs(doc1.longitude) - Math.abs(longitude*10000)), 2)
+      );
+      doc2DistanceToCenter = Math.sqrt(
+        Math.pow(Math.abs(Math.abs(doc2.latitude) - Math.abs(latitude*10000)), 2) + 
+        Math.pow(Math.abs(Math.abs(doc2.longitude) - Math.abs(longitude*10000)), 2)
+      );
+      return doc1DistanceToCenter - doc2DistanceToCenter
+    });
+
     var tokenIds = new Array;
     var allTokensData = new Array;
 
