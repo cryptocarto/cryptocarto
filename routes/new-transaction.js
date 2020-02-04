b17d737a66164423b6597a27076b33861105eae3
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2020-02-04 19:25:24 +0100

    Saves token in DB while waiting for blockchain

--- routes/new-transaction.js
+++ routes/new-transaction.js
@@ -5,6 +5,7 @@
 // Get required interfaces
 const caver = require('../utils/caver')
 const CryptoCartoContract = require('../utils/cryptocarto-contract')
+const PinToken = require('../utils/pintoken')
 
 module.exports = async function(req, res, next) {
   try {
@@ -35,27 +36,66 @@ module.exports = async function(req, res, next) {
       data: CryptoCartoContract.methods.mintPinToken(message, latitude, longitude).encodeABI(),
       value: caver.utils.toPeb('0', 'KLAY'), //0.00001
     }, req.session.privatekey);
+
+    // Get new token ID by computing latitude and longitude for id format
+    idFormattedLatitude = latitude;
+    if (latitude < 0) { idFormattedLatitude = (0 - latitude) + 1000000; }
+    idFormattedLongitude = longitude;
+    if (longitude < 0) { idFormattedLongitude = (0 - longitude) + 10000000; }
+    newTokenId = (idFormattedLatitude * 100000000) + idFormattedLongitude;
     
-    // Send transaction through fee delegation
-    await caver.klay.sendTransaction({
+    // Create token in DB (before blockchain confirm, will be erased if needed)
+    var newPinToken = new PinToken({
+        tokenId : newTokenId,
+        owner: req.session.address,
+        latitude : latitude,
+        longitude : longitude,
+        message : message,
+        timestamp : Math.round(Date.now() / 1000)
+    });
+    
+    // Saves if not existing
+    if (!await PinToken.countDocuments({ tokenId: newPinToken.tokenId })) {
+      await newPinToken.save();
+      console.log("PinToken #" + newPinToken.tokenId + " saved to DB (temporary timestamp: " + newPinToken.timestamp + ").")
+    } else {
+      throw new Error("Token #" + newPinToken.tokenId + " already exists.")
+    };
+    
+    // Send transaction through fee delegation (async)
+    caver.klay.sendTransaction({
       senderRawTransaction: senderRawTransaction,
       feePayer: process.env.FEE_PAYER_ADDRESS,
     })
     .on('transactionHash', function(hash){
-        console.log('transactionHash', hash);
+        console.log('Transaction Hash received: Tx hash is ', hash);
     })
-    .on('receipt', function(receipt){
-        console.log('receipt', receipt);
-        console.log('Tx hash is '+ receipt.transactionHash);
-        console.log('Sender Tx hash is '+ receipt.senderTxHash);
+    .on('receipt', async function(receipt){
+        console.log('Receipt received: Sender Tx hash is '+ receipt.senderTxHash);
+
+        // Replace current temp token in DB by blockchain values (only timestamp should change)
+        CryptoCartoContract.methods.getPinToken(newPinToken.tokenId).call().then(async function(tokenDataFromBC) {
+            var newPinTokenFromBC = new PinToken({
+              tokenId : tokenDataFromBC[0],
+              owner: tokenDataFromBC[1],
+              latitude : tokenDataFromBC[2],
+              longitude : tokenDataFromBC[3],
+              message : tokenDataFromBC[4],
+              timestamp : tokenDataFromBC[5]
+          });
+          await PinToken.deleteMany({ tokenId: newPinToken.tokenId });
+          await newPinTokenFromBC.save();
+          console.log("PinToken #" + newPinToken.tokenId + " swapped with blockchain values in DB (new timestamp: " + newPinTokenFromBC.timestamp + ").")
+        });
     })
-    .on('error', console.error);
+    .on('error', function(error){
+      // On error, delete the pin in DB and log
+      PinToken.deleteMany({ tokenId: newPinToken.tokenId });
+      console.error(error);
+    });
 
-    req.session.generalMessage = 'Transaction was created on blockchain.';
-
-    // Updates the PinTokens DB
-    updatePinTokensDB = require('../utils/update-pin-tokens-db')
-    await updatePinTokensDB();
+    req.session.generalMessage = 'Token #' + newPinToken.tokenId + ' created.';
+    req.session.openPinId = newPinToken.tokenId;
 
     res.redirect('/');
 
