0c37764f5e49fa1b54be6ea4b7bab9354b04ce3d
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2021-03-07 15:12:54 +0100

    Added support for KAS endpoint

--- utils/caver.js
+++ utils/caver.js
@@ -4,7 +4,20 @@
 
 //Setting up Klaytn data
 const Caver = require('caver-js')
-const caver = new Caver(process.env.CAVER_PROVIDER)
+var caver;
+
+// Special treatment when using KAS
+if (process.env.CAVER_PROVIDER == "KAS") {
+    const option = {
+        headers: [
+            {name: 'Authorization', value: 'Basic ' + Buffer.from(process.env.KAS_API_KEYID + ':' + process.env.KAS_API_ACCESSKEY).toString('base64')},
+            {name: 'x-chain-id', value: process.env.KAS_API_CHAINID},
+        ]
+    }
+    caver = new Caver(new Caver.providers.HttpProvider(process.env.KAS_API_ENDPOINT, option));
+} else {
+    caver = new Caver(process.env.CAVER_PROVIDER)
+}
 
 // Setting up fee delegation structure
 caver.klay.accounts.wallet.add(process.env.FEE_PAYER_PRIVATE_KEY, process.env.FEE_PAYER_ADDRESS);
