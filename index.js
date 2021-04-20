1e0d5f2d0b084872cea1f5629a3497bbc36e472a
Antoine CryptoCarto <antoine@cryptocarto.xyz>
2021-04-20 14:10:46 +0200

    Corrected db update on external transfer or update

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ if (process.env.ENVIRONMENT != 'dev') {
   app.set('trust proxy', 1); // Trusts 1 proxy for secure HHTPS cookiein production
 };
 
-app.use(session({ 
-  secret: process.env.SESSION_SECRET, 
-  resave: false, 
-  saveUninitialized: false, 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
   store: new MongoStore({ mongooseConnection: db.connection, secret: process.env.SESSION_SECRET }),
   rolling: true,
-  cookie: { 
+  cookie: {
     secure: (process.env.ENVIRONMENT != 'dev'), // Use a secure HTTPS-only cookie if not in dev
     maxAge: 1209600000 // 2 weeks duration
   }
@@ -45,7 +45,7 @@ app.use(session({
 
 // Redirect to HTTPS
 if (process.env.ENVIRONMENT != 'dev') {
-  app.get('*', function(req, res, next) {
+  app.get('*', function (req, res, next) {
     if (req.headers["x-forwarded-proto"] !== 'https') {
       res.redirect('https://' + req.headers.host + req.url);
       return;
@@ -67,84 +67,92 @@ const CryptoCartoContract = require('./utils/cryptocarto-contract')
 var bcWatcherDelay = process.env.BC_WATCHER_DELAY || 30;
 var bcWatcherDelayWithMargin = parseInt(bcWatcherDelay) + Math.ceil(bcWatcherDelay / 10);
 console.log("Delays: " + bcWatcherDelay + " / " + bcWatcherDelayWithMargin);
-setInterval(async function() {
+setInterval(async function () {
   try {
-    await caver.klay.getBlockNumber().then(function(latestBlockNumber){
+    await caver.klay.getBlockNumber().then(function (latestBlockNumber) {
       console.log("Running transfer watcher at block " + latestBlockNumber);
       CryptoCartoContract.getPastEvents('Transfer', {
         fromBlock: latestBlockNumber - bcWatcherDelayWithMargin, // Get events since last watch
-        toBlock: 'latest'}
-      , function(error, events){
-        try {
-          // Updating owner for token
-          events.forEach(event => {
-            tokenIdToRemove = event.returnValues.tokenId;
-            console.log("Updating token ID #" + event.returnValues.tokenId + " (transfer detected)");
-            PinToken.updateMany({ tokenId: event.returnValues.tokenId }, { $set: { owner: event.returnValues.to } })
-          });
-        } catch (error) { console.error("Error while crawling events.") }
-      })
+        toBlock: 'latest'
+      }
+        , function (error, events) {
+          try {
+            // Updating owner for token
+            events.forEach(async event => {
+              tokenIdToRemove = event.returnValues.tokenId;
+              console.log("Updating token ID #" + event.returnValues.tokenId + " (transfer detected)");
+              await PinToken.updateMany({ tokenId: event.returnValues.tokenId }, { $set: { owner: event.returnValues.to } })
+              console.log(event);
+              console.log("token id: " + event.returnValues.tokenId)
+              console.log("to: " + event.returnValues.to)
+            });
+          } catch (error) { console.error("Error while crawling events.") }
+        })
 
       CryptoCartoContract.getPastEvents('PinTokenModified', {
         fromBlock: latestBlockNumber - bcWatcherDelayWithMargin, // Get events since last watch
-        toBlock: 'latest'}
-      , function(error, events){
-        try {
-          // Updating message and modification date for token
-          events.forEach(event => {
-            console.log("Updating token ID #" + event.returnValues.tokenId + " (message updated)");
-            PinToken.updateMany({ tokenId: event.returnValues.tokenId }, { $set: { message: event.returnValues.message, modificationTimestamp: event.returnValues.timestamp } })
-          });
-        } catch (error) { console.error("Error while crawling events.") }
-      })
+        toBlock: 'latest'
+      }
+        , function (error, events) {
+          try {
+            // Updating message and modification date for token
+            events.forEach(async event => {
+              console.log("Updating token ID #" + event.returnValues.tokenId + " (message updated)");
+              await PinToken.updateMany({ tokenId: event.returnValues.tokenId }, { $set: { message: event.returnValues.message, modificationTimestamp: event.returnValues.timestamp } })
+            });
+          } catch (error) { console.error("Error while crawling events.") }
+        })
 
       CryptoCartoContract.getPastEvents('ConsumptionRightsChanged', {
         fromBlock: latestBlockNumber - bcWatcherDelayWithMargin, // Get events since last watch
-        toBlock: 'latest'}
-      , function(error, events){
-        try {
-          events.map(async function(event){
-            var address = event.returnValues.owner.toLowerCase();
-            var currentRights = await ConsumptionRight.findOne({ address: address });
+        toBlock: 'latest'
+      }
+        , function (error, events) {
+          try {
+            events.map(async function (event) {
+              var address = event.returnValues.owner.toLowerCase();
+              var currentRights = await ConsumptionRight.findOne({ address: address });
 
-            if (!currentRights) {
+              if (!currentRights) {
 
-              //If no DB record, create one with received values and proceed
-              var newConsumptionRights = new ConsumptionRight({
-                address: address,
-                rights: event.returnValues.newConsumptionRights,
-                lastRefillTimestamp: event.returnValues.lastRefillTimestamp,
-                lastChangeTimestamp: event.returnValues.eventTimestamp
-              });
-              await newConsumptionRights.save();
+                //If no DB record, create one with received values and proceed
+                var newConsumptionRights = new ConsumptionRight({
+                  address: address,
+                  rights: event.returnValues.newConsumptionRights,
+                  lastRefillTimestamp: event.returnValues.lastRefillTimestamp,
+                  lastChangeTimestamp: event.returnValues.eventTimestamp
+                });
+                await newConsumptionRights.save();
 
-              console.log("Consumption right for address " + address + " created: " 
-                + event.returnValues.newConsumptionRights + " (with TS: " + event.returnValues.lastRefillTimestamp + ") ");
+                console.log("Consumption right for address " + address + " created: "
+                  + event.returnValues.newConsumptionRights + " (with TS: " + event.returnValues.lastRefillTimestamp + ") ");
 
-            } else if (currentRights.lastChangeTimestamp < event.returnValues.eventTimestamp) {
+              } else if (currentRights.lastChangeTimestamp < event.returnValues.eventTimestamp) {
 
-              //If the event is more recent than last change, update DB
-              await ConsumptionRight.updateMany({ address: address }, { $set: {
-                rights: event.returnValues.newConsumptionRights,
-                lastRefillTimestamp: event.returnValues.lastRefillTimestamp,
-                lastChangeTimestamp: event.returnValues.eventTimestamp,
-              }});
+                //If the event is more recent than last change, update DB
+                await ConsumptionRight.updateMany({ address: address }, {
+                  $set: {
+                    rights: event.returnValues.newConsumptionRights,
+                    lastRefillTimestamp: event.returnValues.lastRefillTimestamp,
+                    lastChangeTimestamp: event.returnValues.eventTimestamp,
+                  }
+                });
 
-              console.log("Consumption right for address " + address + " changed to " 
-                + event.returnValues.newConsumptionRights + " (new TS: " + event.returnValues.lastRefillTimestamp + ") ");
+                console.log("Consumption right for address " + address + " changed to "
+                  + event.returnValues.newConsumptionRights + " (new TS: " + event.returnValues.lastRefillTimestamp + ") ");
 
-            } else if (currentRights.lastChangeTimestamp == event.returnValues.eventTimestamp) {
-              //In this case, state is unknown: reload data from BC
-              updateConsumptionRightsFromBlockchain = require('./utils/update-consumption-rights-from-blockchain')
-              updateConsumptionRightsFromBlockchain(address);
-              console.log("Consumption right for address " + address + " reloading from blockchain");
-            } else {
-              console.log("Consumption right for address " + address + " unchanged, old event received (TS: " + event.returnValues.eventTimestamp +
-              ") - event values are " + event.returnValues.newConsumptionRights + " (new TS: " + event.returnValues.lastRefillTimestamp + ") ");
-            }
-          });
-        } catch (error) { console.error("Error while crawling events.") }
-      })
+              } else if (currentRights.lastChangeTimestamp == event.returnValues.eventTimestamp) {
+                //In this case, state is unknown: reload data from BC
+                updateConsumptionRightsFromBlockchain = require('./utils/update-consumption-rights-from-blockchain')
+                updateConsumptionRightsFromBlockchain(address);
+                console.log("Consumption right for address " + address + " reloading from blockchain");
+              } else {
+                console.log("Consumption right for address " + address + " unchanged, old event received (TS: " + event.returnValues.eventTimestamp +
+                  ") - event values are " + event.returnValues.newConsumptionRights + " (new TS: " + event.returnValues.lastRefillTimestamp + ") ");
+              }
+            });
+          } catch (error) { console.error("Error while crawling events.") }
+        })
     })
     updatePinTokensDB();
   } catch (error) { console.error("Error while watching ERC-721 transfers") }
@@ -159,24 +167,24 @@ updatePinTokensDB();
 
 // View a specific pin
 app.get('/view-pin/:pinid',
-async function(req, res, next) {
-  try {
-    // Get token id from URL
-    var tokenId = parseInt(req.params.pinid);
+  async function (req, res, next) {
+    try {
+      // Get token id from URL
+      var tokenId = parseInt(req.params.pinid);
 
-    //Get tokenData
-    tokenData = await CryptoCartoContract.methods.getPinToken(tokenId).call();
+      //Get tokenData
+      tokenData = await CryptoCartoContract.methods.getPinToken(tokenId).call();
 
-    // Set poition session variables
-    req.session.currentlat = parseFloat(tokenData[3]) / 10000;
-    req.session.currentlng = parseFloat(tokenData[4]) / 10000;
-    req.session.openPinId = tokenId;
+      // Set poition session variables
+      req.session.currentlat = parseFloat(tokenData[3]) / 10000;
+      req.session.currentlng = parseFloat(tokenData[4]) / 10000;
+      req.session.openPinId = tokenId;
 
-    req.session.generalMessage = 'Viewing Pin #' + tokenId;
-    req.url = "/";
-    app.handle(req, res, next);
-  } catch (error) { next(error) }
-});
+      req.session.generalMessage = 'Viewing Pin #' + tokenId;
+      req.url = "/";
+      app.handle(req, res, next);
+    } catch (error) { next(error) }
+  });
 
 // Add other routes
 app.use(require("./routes"));
@@ -187,6 +195,6 @@ app.use(function (err, req, res, next) {
   res.redirect('/');
 })
 
-app.listen(port, function(){
-	console.log("CryptoCarto now UP on port "+port);
+app.listen(port, function () {
+  console.log("CryptoCarto now UP on port " + port);
 });
